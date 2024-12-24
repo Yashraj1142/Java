85066db12f2d3a65810c138de490d61195ff0ec9
Yashraj Kumar Gupta <67089868+Yashraj1142@users.noreply.github.com>
2024-12-24 17:42:32 +0530

    String Manipulation

---
+++ String Manipulation/newKeyword.java
@@ -0,0 +1,12 @@
+class newKeyword {
+    public static void main(String args[]){
+        String s1 = new String("Hello");
+        String s2 = new String("Hello");
+        char ch[] = {'H', 'e', 'l', 'l', 'o'};
+        String s3 = new String(ch);
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
